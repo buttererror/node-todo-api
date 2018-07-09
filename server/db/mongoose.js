f57bcc1aa7c4abf4ec6356650751326ab5fbfa8c
Mahmoud <sswsy2006@gmail.com>
2018-07-09 18:12:28 +0200

    heroku setup db username fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 let db = {
    localhost: "mongodb://localhost:27017/TodoApp",
-   mlabhost: "mongodb://<buttererror>:<b3Y0urS3lf>@ds231941.mlab.com:31941/mongo-data"
+   mlabhost: "mongodb://<butterface>:<b3Y0urS3lf>@ds231941.mlab.com:31941/mongo-data"
 };
 mongoose.connect(db.mlabhost || db.localhost);
 
