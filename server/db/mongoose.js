56349ef5efb8498c29ffd4ac9fbba17436ea0ac5
Mahmoud <sswsy2006@gmail.com>
2018-07-09 19:16:11 +0200

    auth fixed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
+// let options = {
+//    user: "butterface",
+//    pass: "b3Y0urs3lf",
+//    auth: {
+//       authdb: "magic"
+//    }
+// };
+
 let db = {
    localhost: "mongodb://localhost:27017/TodoApp",
    mlabhost: "mongodb://butterface:b3Y0urS3lf@ds231941.mlab.com:31941/mongo-data"
 };
 mongoose.connect(db.mlabhost || db.localhost);
-
 module.exports = {mongoose};
